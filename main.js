a86244777d15e7239d86275a4a751b7658a93e1c
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-12-12 17:36:57 +0100

    styling modal

---
+++ main.js
@@ -0,0 +1,18 @@
+const addBtn = add__btn;
+// modal work
+//1.
+
+// add new contact
+// 1. input
+// 2. btn - save --> display input
+// 3. btn - cancel
+
+// display contact
+
+// delete
+
+// edit
+
+// search
+
+// local storage
